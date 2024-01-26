e8e90be7d6b543ad37b802d58559124abf80a150
Englishman <56271563+RomanEnglishman@users.noreply.github.com>
2024-01-26 15:03:57 +0600

    123

--- lab_2/lib/main.dart
+++ lab_2/lib/main.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,7 +6,7 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -15,19 +16,136 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.pink,
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text(
+          "MYAPP",
+          style: TextStyle(
+            color: Colors.white, // Цвет текста
+          ),
         ),
-        body: Container(
-          color: Colors.purple,
-          height: 200.0, 
-          width: 300.0,
-          child: Center(
-          child: Text("jj"),
+        centerTitle: true, // Центрировать текст
+      ),
+      body: Center(
+        child: Container(
+          alignment: Alignment.center,
+          child: Padding(
+            padding: EdgeInsets.all(40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.deepOrange,
+                    primary: Colors.deepOrange,
+                    fixedSize: Size(100, 100),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text("Button", style: TextStyle(color: Colors.white)),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.black,
+                    primary: Colors.blue,
+                    fixedSize: Size(100, 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 5, // Adjust the elevation value
+                  ),
+                  onPressed: () {},
+                  child: Text("Button", style: TextStyle(color: Colors.white)),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.black,
+                    primary: Colors.blue,
+                    fixedSize: Size(100, 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 5, // Adjust the elevation value
+                  ),
+                  onPressed: () {},
+                  child: Text("Button", style: TextStyle(color: Colors.white)),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: Icon(Icons.add_to_drive_rounded),
+                  label: Text(
+                    "Data",
+                    style: TextStyle(
+                      color: Colors.white, // Цвет текста
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    shadowColor: Colors.black,
+                    primary: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    elevation: 5, // Adjust the elevation value
+                  ),
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shadowColor: Colors.black,
+                        primary: Colors.blue,
+                        fixedSize: Size(100, 40),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        elevation: 5, // Adjust the elevation value
+                      ),
+                      onPressed: () {},
+                      child:
+                          Text("Button", style: TextStyle(color: Colors.white)),
+                    ),
+                    SizedBox(width: 70),
+                    Text(
+                      "Hello world!\nHello world!",
+                      textAlign: TextAlign
+                          .center, // Center the text within the available space
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text("Hello World! This is a Text Widget!",
+                    style: TextStyle(
+                      color: Colors.purple,
+                      backgroundColor: Colors.yellowAccent,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic,
+                      letterSpacing: 8,
+                      shadows: [
+                        Shadow(
+                          color: Colors.grey,
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 3.0,
+                        )
+                      ],
+                    )),
+                SizedBox(height: 20),
+                Icon(Icons.speaker, size: 60, color: Colors.black),
+                SizedBox(width: 7),
+                Text("Text Speaker", style: TextStyle(fontSize: 15)),
+              ],
+            ),
           ),
         ),
       ),
-    );
+    ));
   }
 }
